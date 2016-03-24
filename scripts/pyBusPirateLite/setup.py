dc1528135f671e0b8a32e516cb54a414953b3552
Grey Christoforo <grey@christoforo.net>
2016-03-24 14:48:58 +0000

    created setup.py for pyBusPirateLite

---
+++ scripts/pyBusPirateLite/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(
+    name='BusPirateLite',
+    url='http://dangerousprototypes.com/docs/Bus_Pirate',
+    version='0.1',
+    packages=['pyBusPirateLite',],
+    license='GPL',
+    maintainer='Grey Chistoforo',
+    maintainer_email='first name [at] last name [dot] net'
+)
